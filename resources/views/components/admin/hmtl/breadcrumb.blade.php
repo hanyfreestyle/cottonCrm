941523a840399106a2894dcd300af02f5b4b897e
Hany Darwish <hany.freestyle4u@gmail.com>
2024-08-05 11:27:24 +0300

    Update

--- resources/views/components/admin/hmtl/breadcrumb.blade.php
+++ resources/views/components/admin/hmtl/breadcrumb.blade.php
@@ -1,8 +1,8 @@
 <section class="content-header">
     <div class="container-fluid">
-        <div class="row mb-2">
-            <div class="col-sm-6">
-                <h1 class="def_breadcrumb_h1 text-lg font-weight-lighter">
+        <div class="row">
+            <div class="col-lg-9 col-9">
+                <h1 class="def_breadcrumb_h1">
                     @if($butView)
                         <a href="{{route('admin.Dashboard')}}">
                             <i class="fa {{IsArr($pageData,'IconPage','fa-home')}}"></i>
@@ -11,12 +11,12 @@
                     {{$pageData['TitlePage']}}
                 </h1>
             </div>
-            <div class="col-sm-6">
+            <div class="col-lg-3 col-3">
                 <ol class="breadcrumb float-sm-right text-md">
                     @if ($pageData['ViewType'] == 'List')
                     @else
                         @if(isset($pageData['PageListUrl']))
-                            <x-admin.form.action-button url="{{$pageData['PageListUrl']}}" print-lable="{{$pageData['ListPageName']}}" size="s" bg="p" :tip="false"/>
+                            <x-admin.form.action-button url="{{$pageData['PageListUrl']}}" print-lable="{{$pageData['ListPageName']}}" icon="fas fa-search" size="s" bg="p" :tip="$agent->isMobile()"/>
                         @endif
                     @endif
                 </ol>
