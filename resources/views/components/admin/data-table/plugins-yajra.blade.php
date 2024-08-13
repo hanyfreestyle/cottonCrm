aa05f9e1d7fb48ae177fff520a6f5dc67c26d36c
Hany Darwish <hany.freestyle4u@gmail.com>
2024-08-13 20:26:13 +0300

    Update

--- resources/views/components/admin/data-table/plugins-yajra.blade.php
+++ resources/views/components/admin/data-table/plugins-yajra.blade.php
@@ -1,9 +1,8 @@
 @if($style)
-    <link rel="stylesheet" href="{{ defAdminAssets('plugins/datatables-bs4/css/dataTables.bootstrap4.min.css') }}">
-    {!! (new \App\Helpers\MinifyTools)->setWebAssets('assets/admin/')->MinifyCss('plugins/datatables-responsive/css/responsive.bootstrap4.css','Seo',true) !!}
-    <link rel="stylesheet" href="{{ defAdminAssets('plugins/datatables-buttons/css/buttons.bootstrap4.min.css') }}">
+    {!! $MinifyTools->setAdmin()->MinifyCss('plugins/datatables-bs4/css/dataTables.bootstrap4.min.css',$minType,$reBuild) !!}
+    {!! $MinifyTools->setAdmin()->MinifyCss('plugins/datatables-responsive/css/responsive.bootstrap4.css',$minType,$reBuild) !!}
+    {!! $MinifyTools->setAdmin()->MinifyCss('plugins/datatables-buttons/css/buttons.bootstrap4.min.css',$minType,$reBuild) !!}
 @endif
-
 
 @if($jscode)
     <script src="{{defAdminAssets('plugins/datatables/jquery.dataTables.min.js')}}"></script>
