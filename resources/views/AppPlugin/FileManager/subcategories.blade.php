9dd2239b8fab69a2b4e702a20aa16bf8ce8ff3aa
Hany Darwish <hany.freestyle4u@gmail.com>
2024-08-18 16:52:01 +0300

    Update

--- resources/views/AppPlugin/FileManager/subcategories.blade.php
+++
@@ -1,9 +0,0 @@
-@foreach ($subcategories as $sub)
-    <option value="{{ $sub['path'] }}" @if ($sub['path'] == old('path',issetArr($_GET,'path',null)) ) selected @endif >{{ $parent}} > {{ $sub['name'] }}</option>
-    @if (count($sub['children']) > 0)
-        @php
-            $parents = $parent . ' > ' . $sub->name;
-        @endphp
-        @include('AppPlugin.FileManager.subcategories', ['subcategories' => $sub->children, 'parent' => $parents])
-    @endif
-@endforeach
